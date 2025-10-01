6a754051dde1687f6f535ecd8b56530b46f2e452
rpterminal <luizrenanpontes@gmail.com>
2025-10-01 10:52:02 -0300

    corrc on FMCBool

--- Solutions/FMCBool.hs
+++ Solutions/FMCBool.hs
@@ -2,21 +2,21 @@ module ExBool where
 
 -- Do not alter this import!
 import Prelude
-    ( Show(..)
-    , Eq(..)
-    , Ord(..)
-    , Num(..)
-    , Enum(..)
-    , Integral(..)
-    , Int
-    , Char
-    , (++)
-    , ($)
-    , (.)
-    , undefined
-    , error
-    , otherwise
-    )
+  ( Char,
+    Enum (..),
+    Eq (..),
+    Int,
+    Integral (..),
+    Num (..),
+    Ord (..),
+    Show (..),
+    error,
+    otherwise,
+    undefined,
+    ($),
+    (++),
+    (.),
+  )
 
 -- Define evenerything that is undefined,
 -- without using standard Haskell functions.
@@ -25,27 +25,27 @@ import Prelude
 data Bool = False | True
 
 instance Show Bool where
-    show True  = "True"
-    show False = "False"
+  show True = "True"
+  show False = "False"
 
 instance Enum Bool where
-    toEnum 0   = False
-    toEnum 1 = True
-    toEnum _   = undefined
+  toEnum 0 = False
+  toEnum 1 = True
+  toEnum _ = undefined
 
-    fromEnum False = 0
-    fromEnum True  = 1
+  fromEnum False = 0
+  fromEnum True = 1
 
 -- conjunction (AND)
 (&&) :: Bool -> Bool -> Bool
 True && True = True
-_ && _       = False
+_ && _ = False
 
 infixr 3 &&
 
 -- disjunction (OR)
 (||) :: Bool -> Bool -> Bool
-True || _  = True
+True || _ = True
 False || b = b
 
 infixr 2 ||
@@ -53,54 +53,52 @@ infixr 2 ||
 -- NAND (Sheffer stroke)
 (/|\) :: Bool -> Bool -> Bool
 True /|\ True = False
-_ /|\ _         = False
+_ /|\ _ = False
 
 infixr 2 /|\
 
 -- NOR (aka: Peirce arrow or Quine dagger)
 (\|/) :: Bool -> Bool -> Bool
 False \|/ False = True
-_ \|/ _         = False
+_ \|/ _ = False
 
 infixr 2 \|/
 
 -- XOR (exclusive disjunction)
 (<=/=>) :: Bool -> Bool -> Bool
-True <=/=> True   = False
+True <=/=> True = False
 False <=/=> False = False
-_ <=/=> _         = True
+_ <=/=> _ = True
 
 infixr 2 <=/=>
 
 -- boolean negation
 not :: Bool -> Bool
-not True  = False
+not True = False
 not False = True
 
 -- if-then-else expression
 ifThenElse :: Bool -> a -> a -> a
-ifThenElse True t e  = t
-ifThenElse False t e = else
+ifThenElse True t e = t
+ifThenElse False t e = e
 
 -- logical "implies"
 (==>) :: Bool -> Bool -> Bool
 True ==> False = False
-_ ==> _        = True
+_ ==> _ = True
 
 infixr 1 ==>
 
 -- logical "implied by"
 (<==) :: Bool -> Bool -> Bool
 False <== True = False
-_ <== _        = True
+_ <== _ = True
 
 infixl 1 <==
 
 -- logical equivalence
 (<=>) :: Bool -> Bool -> Bool
-True <=> b  = b
+True <=> b = b
 False <=> b = not b
 
 infixr 1 <=>
-
-
